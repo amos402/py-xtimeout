2415be3df8c9d74b06ea8fb21fb4bb584493238f
amos402 <amos_402@msn.com>
2018-02-20 14:25:57 +0800

    Initialize

---
+++ src/pymonitor.py
@@ -0,0 +1,28 @@
+import functools
+
+from _pymonitor import Injector
+
+__version__ = "0.1"
+__all__ = ["check_context", "check_time", "Injector"]
+
+
+class check_context(object):
+	def __init__(self, timeout, callback):
+		self._injector = Injector(timeout, callback)
+
+	def __enter__(self):
+		self._injector.start()
+		return self
+
+	def __exit__(self, exc_type, exc_value, tb):
+		self._injector.stop()
+
+
+def check_time(timeout, callback):
+	def decorate(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs):
+			with check_context(timeout, callback):
+				return func(*args, **kwargs)
+		return wrapper
+	return decorate
